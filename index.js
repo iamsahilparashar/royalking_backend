ae0707cd12668980ed70b7bb96a04c6f8ac5c6ab
sahil parashar <79538902+iamsahilparashar@users.noreply.github.com>
2023-08-07 12:47:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const cors = require('cors');
 const port = process.env.PORT ||  5000;
 
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 connectToMongo();
 
